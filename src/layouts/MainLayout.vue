9293feafb910fa013f02c16488c8e34182abe4d9
lisong <1990691193@qq.com>
2021-11-17 08:00:08 +0000

    17

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,41 +2,142 @@
   <q-layout view="hHh lpR fFf" class="bg-grey-1">
     <q-header elevated class="bg-white text-grey-8 q-py-xs">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-btn flat no-caps no-wrap v-if="$q.screen.gt.xs">
+          <q-icon :name="fabYoutube" color="red" size="28px" />
+          <q-toolbar-title shrink class="q-ml-xs">YouTube</q-toolbar-title>
+        </q-btn>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-space />
+
+        <div class="YL__toolbar-input-container row no-wrap">
+          <q-input
+            dense
+            outlined
+            square
+            v-model="search"
+            placeholder="YouTube"
+            class="bg-white col"
+          />
+          <q-btn
+            icon="search"
+            class="YL__toolbar-input-btn"
+            color="grey-3"
+            text-color="grey-8"
+            unelevated
+          />
+        </div>
+
+        <q-space />
+
+        <div class="q-gutter-sm row items-center no-wrap">
+          <q-btn round dense flat class="grey-8" icon="video_call" v-if="$q.screen.gt.sm">
+            <q-tooltip>Create a video or post</q-tooltip>
+          </q-btn>
+          <q-btn round dense flat class="grey-8" icon="apps" v-if="$q.screen.gt.sm">
+            <q-tooltip>Apps</q-tooltip>
+          </q-btn>
+          <q-btn round dense flat class="grey-8" icon="message" v-if="$q.screen.gt.sm">
+            <q-tooltip>Messages</q-tooltip>
+          </q-btn>
+          <q-btn round dense flat class="grey-8" icon="notifications">
+            <q-badge color="red" text-color="white" floating>2</q-badge>
+            <q-tooltip>Notifications</q-tooltip>
+          </q-btn>
+          <q-btn round flat>
+            <q-avatar size="26px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+            <q-tooltip>Account</q-tooltip>
+          </q-btn>
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-2" :width="240">
+      <q-scroll-area class="fit">
+        <q-list padding>
+          <template v-for="link in links1" :key="link.text">
+            <q-expansion-item
+              v-if="link.children"
+              :icon="link.icon"
+              :label="link.text"
+              expand-separator
+            >
+              <template v-slot:header>
+                <q-item-section avatar>
+                  <q-icon color="grey" :name="link.icon" />
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>{{ link.text }}</q-item-label>
+                </q-item-section>
+              </template>
+              <q-list padding>
+                <q-item
+                  v-for="c in link.children"
+                  :key="c.text"
+                  class="q-ml-md"
+                  v-ripple
+                  clickable
+                >
+                  <q-item-section avatar>
+                    <q-icon color="grey" :name="c.icon" />
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>{{ c.text }}</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-expansion-item>
+            <q-item v-else  v-ripple clickable>
+              <q-item-section avatar>
+                <q-icon color="grey" :name="link.icon" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>{{ link.text }}</q-item-label>
+              </q-item-section>
+            </q-item>
+          </template>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+          <q-separator class="q-my-md" />
+
+          <q-item v-for="link in links2" :key="link.text" v-ripple clickable>
+            <q-item-section avatar>
+              <q-icon color="grey" :name="link.icon" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>{{ link.text }}</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator class="q-mt-md q-mb-xs" />
+
+          <q-item-label header class="text-weight-bold text-uppercase" >
+            More for YouTube
+          </q-item-label>
+
+           <q-item v-for="link in links3" :key="link.text" v-ripple clickable>
+            <q-item-section avatar>
+              <q-icon color="grey" :name="link.icon" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>{{ link.text }}</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator class="q-my-md" />
+
+           <q-item v-for="link in links4" :key="link.text" v-ripple clickable>
+            <q-item-section avatar>
+              <q-icon color="grey" :name="link.icon" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>{{ link.text }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
@@ -46,72 +147,77 @@
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+import { fabYoutube } from '@quasar/extras/fontawesome-v5'
 
 import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  setup () {
+  setup() {
     const leftDrawerOpen = ref(false)
-
+    const search = ref('')
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
+      toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      },
+      search,
+      fabYoutube,
+      links1: [
+        { icon: 'home', text: 'Home', children: [  { icon: 'whatshot', text: 'Trending' }, { icon: 'subscriptions', text: 'Subscriptions' } ] },
+        { icon: 'whatshot', text: 'Trending' },
+        { icon: 'subscriptions', text: 'Subscriptions' }
+      ],
+      links2: [
+        { icon: 'folder', text: 'Library' },
+        { icon: 'restore', text: 'History' },
+        { icon: 'watch_later', text: 'Watch later' },
+        { icon: 'thumb_up_alt', text: 'Liked videos' }
+      ],
+      links3: [
+        { icon: fabYoutube, text: 'YouTube Premium' },
+        { icon: 'local_movies', text: 'Movies & Shows' },
+        { icon: 'videogame_asset', text: 'Gaming' },
+        { icon: 'live_tv', text: 'Live' }
+      ],
+      links4: [
+        { icon: 'settings', text: 'Settings' },
+        { icon: 'flag', text: 'Report history' },
+        { icon: 'help', text: 'Help' },
+        { icon: 'feedback', text: 'Send feedback' }
+      ],
+      buttons1: [
+        { text: 'About' },
+        { text: 'Press' },
+        { text: 'Copyright' },
+        { text: 'Contact us' },
+        { text: 'Creators' },
+        { text: 'Advertise' },
+        { text: 'Developers' }
+      ],
+      buttons2: [
+        { text: 'Terms' },
+        { text: 'Privacy' },
+        { text: 'Policy & Safety' },
+        { text: 'Test new features' }
+      ]
     }
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.YL {
+  &__toolbar-input-container {
+    min-width: 100px;
+    width: 55%;
+  }
+
+  &__toolbar-input-btn {
+    border-radius: 0;
+    border-style: solid;
+    border-width: 1px 1px 1px 0px;
+    border-color: rgba(0, 0, 0, 0.24);
+  }
+}
+</style>
