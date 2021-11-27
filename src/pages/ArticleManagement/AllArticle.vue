7963e9fb48ddaff84b7cca114d16439d70f9d92a
glassy-sky-lisong <1990691193@qq.com>
2021-11-27 18:51:07 +0800

    Changes

--- src/pages/ArticleManagement/AllArticle.vue
+++ src/pages/ArticleManagement/AllArticle.vue
@@ -2,12 +2,21 @@
   <q-page padding>
     <div class='row'>
       <q-table
+        class='full-width'
         :columns='columns'
         :rows='datas'
         row-key='title'
         title='全部文章'
+        :filter='filter'
       >
-
+        <template v-slot:top-left>
+          <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </template>
+        </template>
       </q-table>
     </div>
     all article
@@ -16,14 +25,16 @@
 
 <script lang='ts'>
 import { column as columns, data as datas } from './tableData'
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default  defineComponent({
   name: 'AllArticle',
   setup () {
+    const filter = ref('')
     return {
       columns,
-      datas
+      datas,
+      filter,
     }
   }
 })
